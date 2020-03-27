e0dd72a3fb920f1f4be40e20da55432480e1df01
Lucas Teles <teleslucas@ymail.com>
2020-03-27 18:58:09 -0300

    ajustes links

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,9 @@
 <template>
   <Layout class="home">
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Sticker House" src="~/stickerhouse-logo.png" />
+    <g-image alt="Sticker House" src="~/stickerhouse-logo.png"/>
     <h1>Sticker House</h1>
-    <p>Nossa loja virtual encontra-se em desenvovimento, e em breve estará disponivel para que você possa adiquirir seus stickers favoritos com toda a facilidade, e sem precisar sair de sua casa!</p>
+    <p>Nossa loja virtual encontra-se em desenvovimento. Em breve estaremos disponiveis para que você possa adiquirir seus stickers favoritos com toda a facilidade e sem precisar sair de sua casa!</p>
     <p class="home-links">
       <a href="https://instagram.com/stickerhousebr" target="_blank" rel="noopener">Instagram</a>
       <a href="https://facebook.com/stickerhouse.com.br" target="_blank" rel="noopener">Facebook</a>
@@ -25,7 +25,18 @@ export default {
   flex-direction: column;
   align-items: center;
 }
+.home h1 {
+  display: none;
+}
 .home-links a {
   margin-right: 1rem;
+  text-decoration: none;
+  font-weight: 500;
+  color: #333;
+  transition: 0.3s;
+}
+.home-links a:hover {
+  color: #111;
+  text-decoration: underline;
 }
 </style>
