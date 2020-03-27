26040e4bd9a753bbb8e6bdbe1e908b90bf094969
Lucas Teles <teleslucas@ymail.com>
2020-03-27 18:18:02 -0300

    Novo Logo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,9 @@
 <template>
   <Layout class="home">
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135"/>
+    <g-image alt="Sticker House" src="~/stickerhouse-logo.png" width="135"/>
     <h1>Sticker House</h1>
-    <p>Nossa loja encontra-se em desenvovimento, e em breve estará disponivel para que você possa adiquirir seus stickers favoritos com toda a facilidade, e sem precisar sair de sua casa!</p>
+    <p>Nossa loja virtual encontra-se em desenvovimento, e em breve estará disponivel para que você possa adiquirir seus stickers favoritos com toda a facilidade, e sem precisar sair de sua casa!</p>
     <p class="home-links">
       <a href="https://instagram.com/stickerhousebr" target="_blank" rel="noopener">Instagram</a>
       <a href="https://facebook.com/stickerhouse.com.br" target="_blank" rel="noopener">Facebook</a>
